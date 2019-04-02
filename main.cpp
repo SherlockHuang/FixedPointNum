7cbca2fb7f590e9bbbd4593d7f0198a4ea4a57a2
PsCool <hpsmayday@gmail.com>
2019-04-02 10:16:58 +0800

    remove unused functions

--- main.cpp
+++ main.cpp
@@ -45,9 +45,6 @@ class FixedNum {
         float toFloat() const;
         int32_t toInt() const;
         int32_t getValue() const { return value; }
-
-        static int GetExponent(int32_t v);
-        static int32_t GetMantissa(int32_t v);
 };
 
 FixedNum FixedNum::operator+(const FixedNum &rhs) const {
@@ -92,22 +89,6 @@ FixedNum& FixedNum::operator/=(const FixedNum &rhs) {
 FixedNum& FixedNum::operator*=(const FixedNum &rhs) {
     value = (value * rhs.value) >> FLOAT_SIZE;
     return *this;
-}
-
-int FixedNum::GetExponent(int32_t v) {
-    // exponent 
-    int expn = 127 - FLOAT_SIZE;
-    for (int i = 30; i >= 0; i++) {
-        if ((1 << i & v) == 1) {
-            return expn += i;
-        }
-    }
-
-    return 255;
-}
-
-int32_t FixedNum::GetMantissa(int32_t v) {
-    return 0;
 }
 
 float FixedNum::toFloat() const {
